15305744e00dd808b5d351d8c905951adebb908b
Jak <jak@giveon.me>
2024-03-08 05:24:10 -0500

    Made sure to initialize vueprint

--- playgrounds/vueprint-playground-nuxt/app.vue
+++ playgrounds/vueprint-playground-nuxt/app.vue
@@ -3,3 +3,27 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+import { getDebugger } from '@jakguru/vueprint'
+import { useVueprint } from '@jakguru/vueprint/composables/useVueprint'
+const debug = getDebugger('App')
+useVueprint({
+  onBooted: {
+    onTrue: () => debug('Booted is true'),
+    onFalse: () => debug('Booted is false'),
+  },
+  onReady: {
+    onTrue: () => debug('Ready is true'),
+    onFalse: () => debug('Ready is false'),
+  },
+  onAuthenticated: {
+    onTrue: () => debug('Authenticated is true'),
+    onFalse: () => debug('Authenticated is false'),
+  },
+  onPushPermissions: {
+    onTrue: () => debug('PushPermissions is true'),
+    onFalse: () => debug('PushPermissions is false'),
+  },
+})
+</script>
