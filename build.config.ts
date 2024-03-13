2d6466b9aed20dbe3893028a8a95715111f85c70
Jak <jak@giveon.me>
2024-03-13 08:57:10 -0400

    Working on build system

--- build.config.ts
+++ build.config.ts
@@ -1,14 +1,70 @@
 import pkg from './package.json' assert { type: 'json' }
 import { defineBuildConfig } from 'unbuild'
-import sass from 'sass'
 import fs from 'fs-extra'
+import webpack from 'webpack'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 const customExternals: Array<string> = ['#app', '@nuxt/schema', '#imports', 'nuxt', 'nuxt/app']
-const includedExternals: Array<string> = ['@mdi/font', '@mdi/font/css/materialdesignicons.css']
+const includedExternals: Array<string> = [
+  '@mdi/font',
+  '@mdi/font/css/materialdesignicons.css',
+  'tiny-emitter',
+]
 const externalsWithCustoms = pkg.dependencies
   ? [...customExternals, ...Object.keys(pkg.dependencies)]
   : [...customExternals]
 const externals = externalsWithCustoms.filter((external) => !includedExternals.includes(external))
+
+const webpackConfig: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/vueprint.scss',
+  output: {
+    path: new URL('./dist', import.meta.url).pathname,
+    filename: 'style.js',
+    assetModuleFilename: 'assets/[hash][ext][query]', // Customize as needed
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext][query]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'vueprint.css',
+    }),
+  ],
+}
+
 export default defineBuildConfig({
   failOnWarn: false,
   name: pkg.name,
@@ -21,12 +77,25 @@ export default defineBuildConfig({
         (entry) => !entry.input || !entry.input.endsWith('src/vueprint')
       )
     },
-    'mkdist:done': () => {
-      const compiledSass = sass.compile('./src/vueprint.scss', {
-        style: 'compressed',
-        loadPaths: ['./node_modules/'],
+    'mkdist:done': async () => {
+      const res = await new Promise((resolve, reject) => {
+        webpack(webpackConfig, (err, stats) => {
+          if (err || stats?.hasErrors()) {
+            return reject(
+              err ||
+                stats?.toString({
+                  colors: true,
+                })
+            )
+          }
+          resolve(
+            stats!.toString({
+              colors: true,
+            })
+          )
+        })
       })
-      fs.writeFileSync('dist/vueprint.css', compiledSass.css, { encoding: 'utf-8' })
+      console.log(res)
     },
     'build:done': () => {
       const nuxtRelatedFiles = fs.readdirSync('./dist/nuxt')
